cb85d9d2a3f9d09da781ae6c816434ef4ce4920e
Diogo S. Martins <santana.martins@ufabc.edu.br>
2022-08-03 16:06:00 -0300

    timer finished

--- src/main/java/FXMLController.java
+++ src/main/java/FXMLController.java
@@ -1,14 +1,99 @@
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
 
 public class FXMLController {
 
     @FXML
-    private Label label;
+    private Button startStopButton;
 
+    @FXML
+    private Label timeLabel;
+
+    private boolean isRunning;
+    private long lastTick;
+    private long currentTime;
+    private final PomodoroTimer timer;
+    private final MediaPlayer player;
+
+    public FXMLController() {
+        timer = new PomodoroTimer();
+
+        var path = getClass().getResource("clock-sound-effect.mp3").toExternalForm();
+        var media = new Media(path);
+
+        player = new MediaPlayer(media);
+    }
+
+    @FXML
     public void initialize() {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+        startStopButton.setOnMouseClicked((ev) -> {
+            if (isRunning) {
+                stopTimer();
+                startStopButton.setText("START");
+            } else {
+                startTimer();
+                startStopButton.setText("CANCEL");
+            }
+        });
+        reset();
+    }
+
+    private void startTimer() {
+        isRunning = true;
+        lastTick = System.currentTimeMillis();
+        timer.start();
+    }
+
+    private void stopTimer() {
+        reset();
+        timer.stop();
+    }
+
+    private void reset() {
+        isRunning = false;
+        lastTick = 0;
+        currentTime  = 25 * 60 * 1000;
+        startStopButton.setText("START");
+        updateUi();
+    }
+
+    private void updateUi() {
+        var minutes = currentTime / 1000 / 60;
+        var seconds = currentTime / 1000 % 60;
+        var formattedTime = String.format("%02d:%02d", minutes, seconds);
+
+        timeLabel.setText(formattedTime);
+    }
+
+    private class PomodoroTimer extends AnimationTimer {
+
+        @Override
+        public void handle(long now) {
+            if (!isRunning) {
+                return;
+            }
+            
+            var delta = System.currentTimeMillis() - lastTick;
+
+            if (delta < 1000) {
+                return;
+            }
+            lastTick = System.currentTimeMillis();
+            currentTime -= 1000;
+            if (currentTime <= 0) {
+                player.setOnEndOfMedia(() -> {
+                    player.stop();
+                    player.seek(Duration.ZERO);
+                });
+                player.play();
+                stopTimer();
+            }
+            updateUi();
+        }
     }
 }
